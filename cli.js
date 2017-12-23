64ad0a73703b56a0b3fc5c527f5b3c63a92c794d
David Stolarsky <david.stolarsky@gmail.com>
2017-12-23 12:08:48 -0500

    slow but works

---
+++ cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const argv = require('yargs').argv;
+const path = require('path');
+
+if(argv._[0] === 'slitscan') {
+   const slitscan = require('./slitscan');
+ 
+   const imagesPath = argv.inputImagesPath;
+   const outputDir = argv.output;
+
+   fs.readdir(imagesPath, (err, files) => {
+    //   console.log('got files:', files);
+      slitscan({imagePaths: files.map(file => path.join(imagesPath, file)), outputDir});
+   });
+}
---
+++ slitscan.js
@@ -0,0 +1,111 @@
+const async = require('async');
+const sharp = require('sharp');
+const path = require('path');
+const sprintf = require('sprintf-js').sprintf;
+
+module.exports = ({ imagePaths, outputDir }) => {
+
+      let height;
+      let tileWidth;
+      let slitIndex = 0;
+      let tileImage;
+      let extractColumn;
+      
+      let slitWidth = 20;
+
+      const copyNextSlitToMural = image => new Promise((resolve, reject) => {
+            
+            const muralColumn = slitIndex * slitWidth;
+            const tileColumn = muralColumn % tileWidth;
+            console.log(tileColumn + ' / ' + tileWidth);
+
+            if(tileColumn === 0) {
+                  // save current tile if exists
+                  if(slitIndex !== 0) {
+                        const tileIndex = muralColumn / tileWidth;
+                        const outputPath = path.join(outputDir, sprintf('tile-%05d.png', tileIndex));
+                        
+                        tileImage
+                              .toFile(outputPath, (err, info) => {
+                                    if(err) {
+                                          console.error('toFile errored: ', err);
+                                          reject(err);
+                                    }
+                                    else {
+                                          console.log('Saved ' + outputPath);
+                                    }
+                              });
+                  }
+                  
+                  // create new tile
+                  tileImage = sharp({
+                        create: {
+                              width: tileWidth,
+                              height: height,
+                              channels: 4,
+                              background: { r: 255, g: 255, b: 255, alpha: 0 }
+                        }
+                  });
+            }
+            
+            image
+                  .extract({left: extractColumn, top: 0, width: slitWidth, height: height})
+                  .toBuffer((err, buffer, info) => {
+                        if(err) {
+                              reject(err);
+                        }
+                        else {
+                              // console.log('pasting to ', {tileColumn});
+                              tileImage = tileImage
+                                    .overlayWith(buffer, {
+                                          top: 0,
+                                          left: tileColumn
+                                          // left: 0
+                                    })
+                                    .png()
+                                    .toBuffer((err, buffer, info) => {
+                                          if(err) {
+                                                reject(err);
+                                          }
+                                          else {
+                                                tileImage = sharp(buffer);
+                                                slitIndex = slitIndex + 1;
+                                                resolve();
+                                          }
+                                    });
+                        }
+
+                  });
+      });
+
+      async.eachSeries(imagePaths, (imagePath, callback) => {
+            const image = sharp(imagePath);
+            image
+                  .metadata()
+                  .then(metadata => {
+                        // console.log(metadata.height);
+                        if(!height) {
+                              height = metadata.height;
+                              tileWidth = height;
+                              // tileWidth = 50;
+                              // extractColumn = Math.round(metadata.width / 2);
+                              extractColumn = 0;
+                              console.log('height = ' + height);
+                        }
+                        copyNextSlitToMural(image)
+                        .then(() => {
+                              callback();
+                        })
+                        .catch(err => {
+                              throw err;
+                        });
+                  })
+                  .catch(err => {
+                        console.error(imagePath, ':', err);
+                        callback();
+                  });
+      }, err => {
+
+      });
+};
+
